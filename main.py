0d3223b555406aa2b08f68b3a9ed20b947ce316d
RakeemRanger <84100634+RakeemRanger@users.noreply.github.com>
2025-10-14 21:25:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import asyncio
+import os
+
+from semantic_kernel import Kernel
+from semantic_kernel.contents.chat_history import ChatHistory
+from semantic_kernel.connectors.ai.anthropic import (
+    AnthropicChatCompletion,
+    AnthropicChatPromptExecutionSettings
+)
+model = "claude-sonnet-4-5"
+api_key = os.getenv("ANTHROPIC_API_KEY")
+
+
+async def chat_with_ai() -> str:
+    bot = AnthropicChatCompletion(
+        ai_model_id=model,
+        api_key=api_key
+    )
+    kernel = Kernel()
+    chat_history = ChatHistory()
+    execution_setting = AnthropicChatPromptExecutionSettings()
+    try:
+        while True:
+            user_input = input("user:")
+            if user_input.lower in ["exit", "quit"]:
+                break
+            chat_history.add_user_message(user_input)
+            response = await bot.get_chat_message_content(
+                chat_history=chat_history,
+                settings=execution_setting
+            )
+            print(f"Assistant: {response}")
+            chat_history.add_assistant_message(str(response))
+    except:
+        raise Exception("Error")
+    
+def main():
+    asyncio.run(chat_with_ai())
+
+if __name__ == "__main__":
+    main()
